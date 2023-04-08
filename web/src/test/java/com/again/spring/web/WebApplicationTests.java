d1d1b1375fe1cae00a544f2f6fac56e27be29dc0
valgh <val.thirty@gmail.com>
2023-04-08 20:00:56 +0200

    First commit and project setup

---
+++ web/src/test/java/com/again/spring/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.again.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
